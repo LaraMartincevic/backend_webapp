25b36b3f5293b67767875bbb3dce3f7e09dc5270
LaraMartincevic <laramartincevic88@gmail.com>
2022-09-06 18:57:10 +0200

    make comment

--- src/index.js
+++ src/index.js
@@ -12,17 +12,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//registration process
-//data is recieved from fronted and sent to the 'Users' collection in the database
+//rgistracija u tijeku, podatci su zaprimljeni sa fronta
+
 app.post("/register", async (req, res) => {
-  //Logging to console what request has been called.
   console.log("Registration called");
 
-  //connecting to database and required collecion
+  //spajanje na bazu
   let db = await connectDB();
   let users = db.collection("korisniki");
-
-  //creating an object that is sent to database
 
   let user = {
     ime: req.body.ime,
@@ -31,7 +28,7 @@ app.post("/register", async (req, res) => {
     password: req.body.password,
   };
 
-  //sending data into the database
+  //slanje na bazu
   await users.insertOne(user, function (err, res) {
     if (err) throw err;
     console.log("User inserted");
@@ -41,16 +38,14 @@ app.post("/register", async (req, res) => {
   res.send("User registered");
 });
 
-//login process which checks if login is correct with the database
+//provjera logina
 app.post("/login", async (req, res) => {
-  //Logging to console what request has been called.
   console.log("Login called");
 
-  //connecting to the database and required collecion
   let db = await connectDB();
   let users = db.collection("korisniki");
   console.log(req.body);
-  //creating query and options for searching the user in the database
+
   let user_query = {
     email: req.body.email,
     password: req.body.password,
